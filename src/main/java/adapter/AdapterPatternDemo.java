051ce6dc94927a641de97e42c572fdba5e0d4100
kmnemon <kmnemon@163.com>
2021-12-02 19:33:23 +0800

    d

---
+++ src/main/java/adapter/AdapterPatternDemo.java
@@ -0,0 +1,10 @@
+package adapter;
+
+public class AdapterPatternDemo {
+    public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
+
+        audioPlayer.play("mp3", "1");
+        audioPlayer.play("mp4", "1");
+    }
+}
